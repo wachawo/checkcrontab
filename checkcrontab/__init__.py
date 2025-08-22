fc52e545a1fc3ef008233b4041861ab8d7d33f0c
wachawo <wachawo@gmail.com>
2025-08-22 23:13:51 +0100

    Change version to 0.0.1

--- checkcrontab/__init__.py
+++ checkcrontab/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 __author__ = 'Aleksandr Pimenov'
 __email__ = 'wachawo@gmail.com'
 __description__ = "A Python3 script for checking syntax and correctness of crontab files"
